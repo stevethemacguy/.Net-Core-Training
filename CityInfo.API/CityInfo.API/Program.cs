1c651097d9a6f8daebc558a38e45d4cd083623d0
Steven Dunn <Dunn.Steven1@gmail.com>
2017-04-17 20:20:14 -0700

    Project is now able to deploy to Heroku.

--- CityInfo.API/CityInfo.API/Program.cs
+++ CityInfo.API/CityInfo.API/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Hosting;
 
 namespace CityInfo.API
@@ -11,9 +12,13 @@ namespace CityInfo.API
     {
         public static void Main(string[] args)
         {
+            //Needed to deploy to heroku. See http://codersblock.com/blog/how-to-run-net-on-heroku/
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)   //Needed to deploy to heroku. See http://codersblock.com/blog/how-to-run-net-on-heroku/
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
